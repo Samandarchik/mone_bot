eae89f34dbb7c318c27be4fce376db5fd5834081
Samandarchik <samandarik5@email.com>
2026-05-26 19:33:15 +0500

    asd

--- auth.go
+++ auth.go
@@ -7,21 +7,11 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
-
-	"golang.org/x/crypto/bcrypt"
 )
+
 type contextKey string
 
 const userCtxKey contextKey = "user"
-
-func hashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(bytes), err
-}
-
-func checkPassword(password, hash string) bool {
-	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
-}
 
 func generateToken() string {
 	b := make([]byte, 32)
